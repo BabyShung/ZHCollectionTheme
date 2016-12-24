40f0d32071a77957a2a184e81c2ac43f2b45b185
Hao Zheng <babyshung8@gmail.com>
2016-12-23 21:50:15 -0500

    Added thoughts tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 
+//Return to top
 $(document).ready(function() {
-      
-
-  
+	var returnTopID = "#return-to-top";
+	$(window).scroll(function() {
+		if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+			$(returnTopID).fadeIn(200);    // Fade in the arrow
+		} else {
+			$(returnTopID).fadeOut(400);   // Else fade out the arrow
+		}
+	});
+	$(returnTopID).click(function() {      // When arrow is clicked
+		$('body,html').animate({
+			scrollTop : 0                       // Scroll to top of body
+		}, 500);
+	});
 });
